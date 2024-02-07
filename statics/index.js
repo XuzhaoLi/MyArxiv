8d88384f6b0b1586eccd890bdff8adbc448fa349
Xuchen Li <lixuchen2024@ia.ac.cn>
2024-02-07 20:55:37 +0800

    Update index.js

--- statics/index.js
+++ statics/index.js
@@ -24,7 +24,7 @@ function switchTheme(e) {
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+const currentTheme = 'dark';
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme === 'light') {
@@ -36,4 +36,3 @@ const timestamp = document.getElementById("build-timestamp");
 const timestamp_local = new Date(timestamp.getAttribute("datetime")).toLocaleString();
 
 const badge = document.getElementById("build-timestamp-badge");
-// badge.src = `https://img.shields.io/github/workflow/status/mlnlp-world/myarxiv/Update?=${timestamp_local}&style=for-the-badge`
